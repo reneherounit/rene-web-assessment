31ccfa32c0e0f0f005458e44aceccfd67ba77aa9
Rene Rosa <realpasro12@gmail.com>
2025-12-16 15:28:20 -0600

    Task1 completed!

--- frontend/src/components/LoginPopup/LoginPopup.jsx
+++ frontend/src/components/LoginPopup/LoginPopup.jsx
@@ -30,14 +30,18 @@ const LoginPopup = ({setShowLogin}) => {
             newUrl += "/api/user/register"
         }
 
-        const response = await axios.post(newUrl,data);
-
-        if(response.data.success){
-            setToken(response.data.token);
-            localStorage.setItem("token", response.data.token)
-            setShowLogin(false);
-        }else{
-            alert(response.data.message);
+        try {
+            const response = await axios.post(newUrl,data);
+            if(response.data.success){
+                setToken(response.data.token);
+                localStorage.setItem("token", response.data.token)
+                setShowLogin(false);
+            }else{
+                alert(response.data.message);
+            }
+        } catch (error) {
+            const message = error.response?.data?.message || "An error occurred";
+            alert(message);
         }
    }
 
